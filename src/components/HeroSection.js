f5aee84b5b5f566ab31a90aa76b1aec25ebb55d2
akashGHOST <78381341+akashGHOST@users.noreply.github.com>
2021-05-16 20:58:28 +0530

    updated video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,13 +2,14 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import video from './videos/video-2.mp4'
 
 
 
 export default function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='./videos/video-2.mp4' type='video/mp4' autoPlay loop muted />
+            <video src={video} type='video/mp4' autoPlay loop muted />
             <h1> ADVENTURE AWAITS</h1>
             <p>What are waiting for</p>  
             <div className="hero-btns">
